6836d8c55b490e8e21ed1e4db9c25627e33cfa04
boris <bkusminov@mail.ru>
2025-07-13 01:08:28 +0300

    gotovoe

--- 1.5.4.go
+++ 1.5.4.go
@@ -1,29 +1,45 @@
-Напишите программу, которая запрашивает у пользователя ввод строки-формулы, 
-а выводит сообщение о правильности написания круглых скобок, например:
-Пример 1
-строка на вход: (1+1)*(2+2)
-вывод: Скобки расставлены верно, 2 открывающиеся, 2 закрывающиеся
-
-Пример 2
-Строка на вход: ((1+1) + (2+2) ))
-вывод: Скобки расставлены неправильно, 3 открывающиеся, 4 закрывающиеся
-
-Заведите два счетчика
-и если встречаете руну ( или ) - прибавляйте к одному еденицу или ко второму
-потом сравните оба
-bufio os.Stdin
-
 package main
+
 import (
-"fmt"
-"os"
-"bufio"
+	"bufio"
+	"fmt"
+	"os"
 )
-func main () {
-	q:=bufio.NewReader(os.Stdin)
+
+func main() {
+	q := bufio.NewReader(os.Stdin)
 	fmt.Println("Введите формулу: ")
 	input, _ := q.ReadString('\n')
-	fmt.Println("Скобки расставлены верно: ", %d, "открывающиеся", %d "закрывающиеся", )
-	fmt.Println("Скобки расставлены неправильно: ", %d, "открывающиеся", %d "закрывающиеся", )
-	
+	result, openBracketCount, closedBacketCount, err := checkCorrectSequenceBracket(input)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	if result {
+		fmt.Printf("Скобки расставлены верно: , %d, открывающиеся, %d закрывающиеся \n", openBracketCount, closedBacketCount)
+		fmt.Printf("Скобки расставлены неправильно: , %d, открывающиеся, %d закрывающиеся \n", openBracketCount, closedBacketCount)
+	}
+}
+func checkCorrectSequenceBracket(s string) (bool, int, int, error) {
+	var err error = nil
+	flag := true
+	stack := 0
+	runes := []rune(s)
+	openBracketCount, closedBacketCount := 0, 0
+	for i := range runes {
+		if runes[i] == '(' {
+			stack++
+			openBracketCount++
+		} else if runes[i] == ')' {
+			closedBacketCount++
+			if stack == 0 {
+				err = fmt.Errorf("Ставить ')' не закрыв '(' нельзя")
+				flag = false
+			}
+			stack--
+		}
+	}
+	if stack != 0 && flag == true {
+		flag = false
+	}
+	return flag, openBracketCount, closedBacketCount, err
 }
